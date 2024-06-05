94c896fc1a226d7cfac60e41e2c7d9c120e0d1cc
Steven Zheng <szheng.persevere@gmail.com>
2024-06-05 10:19:55 -0500

    deleted alot of the elements in order for presentation

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -7,15 +7,25 @@ export class Home extends Component {
   render() {
     return (
       <div className='mainDiv'>
+        <nav className='Navbar'>
+          <div className="h1-logo">
+              <h1>
+                  Garzonio's Garage
+              </h1>
+          </div>
+          <div className="right-side-nav">
+              <ul>
+                  <li>INSERT TAB</li>
+                  <li>ANOTHER OBJ</li>
+              </ul>
+          </div>
+      </nav>
         <div className="mainFrame">
           <img src={carLogo} alt="" />
         </div>
-        <div className='mainImg'>
-          Garzonio's Workshop
-        </div>
         <div className='navigation'>
           <div>CONTACT US</div>
-          <div><a href="ttps://www.google.com/search?q=24+north+tiption+str+munford+tn&sca_esv=274f1da98f794c68&sxsrf=ADLYWIJkxNCprXitY5Ds03Avc74ImeEKCw%3A1716649703769&ei=5_5RZo_SLti-p84P0tujwAw&ved=0ahUKEwjP4a--iqmGAxVY38kDHdLtCMgQ4dUDCBA&uact=5&oq=24+north+tiption+str+munford+tn&gs_lp=Egxnd3Mtd2l6LXNlcnAiHzI0IG5vcnRoIHRpcHRpb24gc3RyIG11bmZvcmQgdG4yBxAhGKABGAoyBxAhGKABGAoyBxAhGKABGAoyBxAhGKABGAoyBxAhGKABGAoyBxAhGAoYqwJI-CxQswVYjypwA3gAkAEAmAGUAqABkxKqAQUwLjkuNLgBA8gBAPgBAZgCEKAC0RLCAgsQABiwAxiiBBiJBcICCxAAGIAEGLADGKIEwgIFECEYnwXCAgUQIRirApgDAOIDBRIBMSBAiAYBkAYEkgcFMy45LjSgB-F8&sclient=gws-wiz-serp">HOW TO GET TO US</a></div>
+          <div><a target='_blank' href="https://www.google.com/search?q=24+north+tiption+str+munford+tn&sca_esv=274f1da98f794c68&sxsrf=ADLYWIJkxNCprXitY5Ds03Avc74ImeEKCw%3A1716649703769&ei=5_5RZo_SLti-p84P0tujwAw&ved=0ahUKEwjP4a--iqmGAxVY38kDHdLtCMgQ4dUDCBA&uact=5&oq=24+north+tiption+str+munford+tn&gs_lp=Egxnd3Mtd2l6LXNlcnAiHzI0IG5vcnRoIHRpcHRpb24gc3RyIG11bmZvcmQgdG4yBxAhGKABGAoyBxAhGKABGAoyBxAhGKABGAoyBxAhGKABGAoyBxAhGKABGAoyBxAhGAoYqwJI-CxQswVYjypwA3gAkAEAmAGUAqABkxKqAQUwLjkuNLgBA8gBAPgBAZgCEKAC0RLCAgsQABiwAxiiBBiJBcICCxAAGIAEGLADGKIEwgIFECEYnwXCAgUQIRirApgDAOIDBRIBMSBAiAYBkAYEkgcFMy45LjSgB-F8&sclient=gws-wiz-serp">HOW TO GET TO US</a></div>
           <div>WESTERN TENNESSEE</div>
         </div>
         <div className='splitScreen'>
